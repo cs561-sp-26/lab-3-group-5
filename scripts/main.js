9e787a840ca34b6f966d0d2b51ff3e0648a01def
Hao Jun Chen <haojunchen51@gmail.com>
2026-04-21 13:30:00 -0700

    add gloabl focused menu item to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,14 @@ let GlobalDialogClose = null;
 /************************************/
 const GlobalMenuBtn =  document.getElementById("menuBtn"); 
 const GlobalMenuItems = document.querySelectorAll("li[role='menuitem']");
+
+//Note: Per Josh Wulf's blog post, we implement all immutable global variables using
+//immediately invoked function expressions
+const GlobalFocusedMenuItem = (() => {
+    let _focusedMenuItem = 0
+    const Store = {
+        get: () => _focusedMenuItem,
+        set: val => (_focusedMenuItem = val)
+    }
+    return Object.freeze(Store)
+})()
